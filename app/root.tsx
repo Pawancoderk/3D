734bba0c09d24cd2332a31ed25e344e2e8898613
Pawan <imrealpawan@gmail.com>
2026-06-17 07:44:02 +0530

    initialize project architecture with constants, auth-enabled root, home dashboard, and file upload support

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,11 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import { useEffect, useState } from "react";
-import { getCurrentUser } from "../lib/puter.action";
+import {
+  getCurrentUser,
+  signIn as puterSignIn,
+  signOut as puterSignOut,
+} from "../lib/puter.action";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -46,19 +50,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
 const DEFAULT_AUTH_STATE: AuthState = {
   isSignedIn: false,
   userName: null,
-  userId: null
+  userId: null,
 }
 
 export default function App() {
-  const [authState, setAuthState] = useState<AuthState>(DEFAULT_AUTH_STATE)
+  const [authState, setAuthState] = useState<AuthState>(DEFAULT_AUTH_STATE);
 
   const refreshAuth = async () => {
     try {
       const user = await getCurrentUser();
+
       setAuthState({
         isSignedIn: !!user,
         userName: user?.username || null,
-        userId: user?.uuid || null
+        userId: user?.uuid || null,
       });
 
       return !!user;
@@ -68,14 +73,27 @@ export default function App() {
     }
   }
 
+  useEffect(() => {
+    refreshAuth()
+  }, []);
+
   const signIn = async () => {
-    await signIn();
+    await puterSignIn();
     return await refreshAuth();
   }
-  useEffect(() => {
-    refreshAuth();
-  }, []);
-  return <Outlet />;
+
+  const signOut = async () => {
+    puterSignOut();
+    return await refreshAuth();
+  }
+
+  return (
+    <main className="min-h-screen bg-background text-foreground relative z-10">
+      <Outlet
+        context={{ ...authState, refreshAuth, signIn, signOut }}
+      />;
+    </main>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
